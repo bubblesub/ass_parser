5e3bdaceabaf7989bd0b28aab69650626ff5d0f8
rr- <rr-@sakuya.pl>
2021-08-09 16:38:25 +0200

    extract ObservableSequence

---
+++ ass_parser/observable_sequence.py
@@ -0,0 +1,173 @@
+"""ObservableSequence definition."""
+from collections.abc import MutableSequence
+from dataclasses import dataclass
+from typing import Generic, Iterable, TypeVar, Union, overload
+
+from ass_parser.observable import Event, Observable
+
+TItem = TypeVar("TItem")
+
+
+@dataclass
+class ItemRemovalEvent(Event, Generic[TItem]):
+    """Observable sequence item removal event."""
+
+    index: Union[int, slice]
+    items: list[TItem]
+    is_committed: bool
+
+
+@dataclass
+class ItemInsertionEvent(Event, Generic[TItem]):
+    """Observable sequence item insertion event."""
+
+    index: Union[int, slice]
+    items: list[TItem]
+    is_committed: bool
+
+
+class ObservableSequence(MutableSequence[TItem]):
+    """Observable sequence - a sequence that lets consumers to subscribe to
+    collection change events.
+    """
+
+    items_about_to_be_removed = Observable()
+    items_removed = Observable()
+
+    items_about_to_be_inserted = Observable()
+    items_inserted = Observable()
+
+    def __init__(self) -> None:
+        """Initialize self."""
+        super().__init__()
+        self._data: list[TItem] = []
+
+    def __len__(self) -> int:
+        return len(self._data)
+
+    @overload
+    def __getitem__(self, index: int) -> TItem:
+        ...  # pragma: no cover
+
+    @overload
+    def __getitem__(self, index: slice) -> list[TItem]:
+        ...  # pragma: no cover
+
+    def __getitem__(
+        self, index: Union[int, slice]
+    ) -> Union[TItem, list[TItem]]:
+        return self._data[index]
+
+    def __delitem__(self, index: Union[int, slice]) -> None:
+        values: list[TItem]
+        if isinstance(index, slice):
+            values = list(self._data[index])
+        else:
+            values = [self._data[index]]
+
+        self.items_about_to_be_removed.emit(
+            ItemRemovalEvent[TItem](
+                index=index, items=values, is_committed=False
+            )
+        )
+        del self._data[index]
+        self.items_removed.emit(
+            ItemRemovalEvent[TItem](
+                index=index, items=values, is_committed=True
+            )
+        )
+
+    @overload
+    def __setitem__(self, index: int, value: TItem) -> None:
+        ...  # pragma: no cover
+
+    @overload
+    def __setitem__(self, index: slice, value: Iterable[TItem]) -> None:
+        ...  # pragma: no cover
+
+    def __setitem__(
+        self,
+        index: Union[int, slice],
+        value: Union[TItem, Iterable[TItem]],
+    ) -> None:
+        old_values: list[TItem]
+        if isinstance(index, slice):
+            old_values = list(self._data[index])
+        else:
+            old_values = [self._data[index]]
+        if isinstance(value, Iterable):
+            new_values = list(value)
+        else:
+            new_values = [value]
+
+        self.items_about_to_be_removed.emit(
+            ItemRemovalEvent[TItem](
+                index=index, items=old_values, is_committed=False
+            )
+        )
+        self.items_about_to_be_inserted.emit(
+            ItemInsertionEvent[TItem](
+                index=index, items=new_values, is_committed=False
+            )
+        )
+        if isinstance(index, int):
+            assert not isinstance(value, Iterable)
+            self._data[index] = value
+        elif isinstance(index, slice):
+            if not isinstance(value, Iterable):
+                raise TypeError("can only assign an iterable")
+            self._data[index] = value
+
+        self.items_removed.emit(
+            ItemRemovalEvent[TItem](
+                index=index, items=old_values, is_committed=True
+            )
+        )
+        self.items_inserted.emit(
+            ItemInsertionEvent[TItem](
+                index=index, items=new_values, is_committed=True
+            )
+        )
+
+    def insert(self, index: int, value: TItem) -> None:
+        values = [value]
+        self.items_about_to_be_inserted.emit(
+            ItemInsertionEvent[TItem](
+                index=index, items=values, is_committed=False
+            )
+        )
+        self._data.insert(index, value)
+        self.items_inserted.emit(
+            ItemInsertionEvent[TItem](
+                index=index, items=values, is_committed=True
+            )
+        )
+
+    def clear(self) -> None:
+        values = self._data[:]
+        self.items_about_to_be_removed.emit(
+            ItemRemovalEvent[TItem](
+                index=slice(-1), items=values, is_committed=False
+            )
+        )
+        self._data.clear()
+        self.items_removed.emit(
+            ItemRemovalEvent[TItem](
+                index=slice(-1), items=values, is_committed=True
+            )
+        )
+
+    def extend(self, values: Iterable[TItem]) -> None:
+        values = list(values)
+        self.items_about_to_be_inserted.emit(
+            ItemInsertionEvent[TItem](
+                index=slice(-1), items=values, is_committed=False
+            )
+        )
+        for value in values:
+            self._data.append(value)
+        self.items_inserted.emit(
+            ItemInsertionEvent[TItem](
+                index=slice(-1), items=values, is_committed=True
+            )
+        )
---
+++ ass_parser/tests/test_observable_sequence.py
@@ -0,0 +1,191 @@
+"""Tests for the ObservableSequence class."""
+from unittest.mock import Mock
+
+import pytest
+
+from ass_parser.observable_sequence import ObservableSequence
+
+
+class TestSequence(ObservableSequence[int]):
+    """Test ObservableSequence implementation."""
+
+
+def test_append() -> None:
+    """Test basic item appending."""
+    item = 123
+    seq = TestSequence()
+    seq.append(item)
+    assert list(seq) == [item]
+
+
+def test_append_emits_insertion_event() -> None:
+    """Test that basic item appending emits an insertion event."""
+    subscriber1 = Mock()
+    subscriber2 = Mock()
+    item = 123
+    seq = TestSequence()
+    seq.items_about_to_be_inserted.subscribe(subscriber1)
+    seq.items_inserted.subscribe(subscriber2)
+    seq.append(item)
+    subscriber1.assert_called_once()
+    subscriber2.assert_called_once()
+
+
+def test_extend_emits_single_insertion_event() -> None:
+    """Test that batch item appending emits a single insertion event."""
+    subscriber1 = Mock()
+    subscriber2 = Mock()
+    events_to_insert = [123, 234]
+    seq = TestSequence()
+    seq.items_about_to_be_inserted.subscribe(subscriber1)
+    seq.items_inserted.subscribe(subscriber2)
+    seq.extend(events_to_insert)
+    subscriber1.assert_called_once()
+    subscriber2.assert_called_once()
+
+
+def test_length() -> None:
+    """Test basic sequence length."""
+    seq = TestSequence()
+    seq.append(123)
+    assert len(seq) == 1
+
+
+def test_basic_delete() -> None:
+    """Test basic item deleting."""
+    seq = TestSequence()
+    seq.append(123)
+    del seq[0]
+    assert list(seq) == []
+
+
+def test_basic_delete_emits_removal_event() -> None:
+    """Test that basic item deleting emits a removal event."""
+    subscriber1 = Mock()
+    subscriber2 = Mock()
+    seq = TestSequence()
+    seq.append(123)
+    seq.items_about_to_be_removed.subscribe(subscriber1)
+    seq.items_removed.subscribe(subscriber2)
+    del seq[0]
+    subscriber1.assert_called_once()
+    subscriber2.assert_called_once()
+
+
+def test_clear_emits_signle_removal_event() -> None:
+    """Test that clearing items emits a single removal event."""
+    subscriber1 = Mock()
+    subscriber2 = Mock()
+    seq = TestSequence()
+    seq.append(123)
+    seq.append(234)
+    seq.items_about_to_be_removed.subscribe(subscriber1)
+    seq.items_removed.subscribe(subscriber2)
+    seq.clear()
+    subscriber1.assert_called_once()
+    subscriber2.assert_called_once()
+
+
+def test_slice_delete() -> None:
+    """Test slice-based event deleting."""
+    original_items = list(range(4))
+    seq = TestSequence()
+    seq.extend(original_items)
+    del seq[1:3]
+    assert list(seq) == [original_items[0], original_items[3]]
+
+
+def test_slice_delete_emits_removal_event() -> None:
+    """Test that slice-based event deleting emits a removal event."""
+    subscriber1 = Mock()
+    subscriber2 = Mock()
+    original_items = list(range(4))
+    seq = TestSequence()
+    seq.extend(original_items)
+    seq.items_about_to_be_removed.subscribe(subscriber1)
+    seq.items_removed.subscribe(subscriber2)
+    del seq[1:3]
+    subscriber1.assert_called_once()
+    subscriber2.assert_called_once()
+
+
+def test_basic_update() -> None:
+    """Test basic event updating."""
+    event1 = 123
+    event2 = 234
+    seq = TestSequence()
+    seq.append(event1)
+    seq[0] = event2
+    assert list(seq) == [event2]
+
+
+def test_basic_update_emits_removal_and_insertion_events() -> None:
+    """Test that basic event updating emits a removal and an insertion
+    event.
+    """
+    subscriber1 = Mock()
+    subscriber2 = Mock()
+    subscriber3 = Mock()
+    subscriber4 = Mock()
+    event1 = 123
+    event2 = 234
+    seq = TestSequence()
+    seq.append(event1)
+    seq.items_about_to_be_removed.subscribe(subscriber1)
+    seq.items_about_to_be_inserted.subscribe(subscriber2)
+    seq.items_removed.subscribe(subscriber3)
+    seq.items_inserted.subscribe(subscriber4)
+    seq[0] = event2
+    subscriber1.assert_called_once()
+    subscriber2.assert_called_once()
+    subscriber3.assert_called_once()
+    subscriber4.assert_called_once()
+
+
+def test_slice_update() -> None:
+    """Test slice-based event updating."""
+    original_items = list(range(4))
+    new_item = 123
+    seq = TestSequence()
+    seq.extend(original_items)
+    seq[1:3] = [new_item]
+    assert list(seq) == [
+        original_items[0],
+        new_item,
+        original_items[3],
+    ]
+
+
+def test_slice_update_emits_removal_and_insertion_events() -> None:
+    """Test that slice-based event updating emits a removal and an
+    insertion event.
+    """
+    subscriber1 = Mock()
+    subscriber2 = Mock()
+    subscriber3 = Mock()
+    subscriber4 = Mock()
+    original_items = list(range(4))
+    new_item = 123
+    seq = TestSequence()
+    seq.extend(original_items)
+    seq.items_about_to_be_removed.subscribe(subscriber1)
+    seq.items_about_to_be_inserted.subscribe(subscriber2)
+    seq.items_removed.subscribe(subscriber3)
+    seq.items_inserted.subscribe(subscriber4)
+    seq[1:3] = [new_item]
+    subscriber1.assert_called_once()
+    subscriber2.assert_called_once()
+    subscriber3.assert_called_once()
+    subscriber4.assert_called_once()
+
+
+def test_slice_update_not_an_iterable() -> None:
+    """Test slice-based event updating raises a TypeError when trying to
+    assign a non-iterable.
+    """
+    original_items = list(range(4))
+    new_item = 123
+    seq = TestSequence()
+    seq.extend(original_items)
+    with pytest.raises(TypeError):
+        seq[1:3] = new_item  # type: ignore
