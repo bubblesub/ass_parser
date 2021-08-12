bade423678e6df428093b7889c62cdfc4fd3bc58
rr- <rr-@sakuya.pl>
2021-08-12 16:15:08 +0200

    export AssMeta

--- ass_parser/__init__.py
+++ ass_parser/__init__.py
@@ -1,8 +1,10 @@
 """ASS parser main module."""
 from ass_parser.ass_event import AssEvent
 from ass_parser.ass_event_list import AssEventList
+from ass_parser.ass_meta import AssMeta
 
 __all__ = [
     "AssEvent",
     "AssEventList",
+    "AssMeta",
 ]
